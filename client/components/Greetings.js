8c9737dffd8cfde69bb639703bda5e1109dc3075
manomit <manomitmitra.1983@gmail.com>
2017-03-15 01:51:11 +0530

    Second Commit

---
+++ client/components/Greetings.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Greetings extends React.Component {
+  render() {
+    return (
+      <h1>Hello From React</h1>
+    );
+  }
+}
+export default Greetings;
---
+++ webpack.config.dev.js
@@ -0,0 +1,29 @@
+import path from 'path';
+import webpack from 'webpack';
+export default {
+  devtools: 'eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, "/client/index.js")
+  ],
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, "client"),
+        loaders: [ 'react-hot', 'babel' ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '', '.js' ]
+  }
+}
